1b47142535295409a49678026ec31f6715bc63a3
Nicolas Nocete <nicolasnocete@gmail.com>
2024-08-27 20:25:55 -0300

    add max touches

--- src/entities/Brick.js
+++ src/entities/Brick.js
@@ -8,6 +8,7 @@ export class Brick extends Phaser.GameObjects.Rectangle {
     this.body.setCollideWorldBounds(true);
 
     this.toches = 0;
+    this.maxToches = Phaser.Math.Between(1, 4);
   }
 
   hit() {
@@ -15,8 +16,14 @@ export class Brick extends Phaser.GameObjects.Rectangle {
     if (this.toches === 1) {
       this.setFillStyle(0xff0000);
     }
-
     if (this.toches === 2) {
+      this.setFillStyle(0x00ff00);
+    }
+    if (this.toches === 3) {
+      this.setFillStyle(0x0000ff);
+    }
+
+    if (this.toches === this.maxToches) {
       this.destroy();
     }
   }
